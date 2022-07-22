7db94fc914c01b50a48c543308f75b83c9315f34
RiiGanzz <106065864+RiiGanzz@users.noreply.github.com>
2022-07-22 16:39:43 +0700

    Update help.js

--- message/help.js
+++ message/help.js
@@ -2,17 +2,18 @@ exports.txtSewabot = (prefix) => {
 return `
  1-Minggu : Rp5.000
  2-Minggu : Rp.10.000
- 1-Bulan : Rp.20.000
- 2-Bulan : Rp.30.000
- 3-Bulan : Rp40.000
+ 1-Bulan : Rp.15.000
+ 2-Bulan : Rp.20.000
+ 3-Bulan : Rp30.000
 `
 }
 exports.txtDonate = (prefix) => {
 return `
 ▷_*DONASI BOT*_
- _- DANA : 08xxxxxxxx_
- _- OVO : 08xxxxxxxx_
- _- GOPAY : 08xxxxxxxx_
+ _- DANA : 081247822525_
+ _- OVO : 081367417823_
+ _- GOPAY : 081247822525_
  _- QRIS : https://bit.ly/3yy0qoZ_
+ _Thnks For The Support Yh_
 `
 }
